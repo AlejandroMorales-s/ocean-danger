9131addc1aa753f485f82deb154bc987affea0e4
Alejandro <alejandrom.am33@gmail.com>
2022-05-14 17:05:34 -0500

    add: endpoint for search animals by id

--- server.js
+++ server.js
@@ -9,12 +9,20 @@ const { application } = require('express');
 const prisma = new PrismaClient();
 
 app.get('/', (req, res) => {
-    res.json({message: 'alive'});
+    res.json({message: 'Welcome to Ocean in Danger API'});
 });
 
+//* Endpoint para obtener todos los animales
 app.get('/animals', async (req, res) => {
     const allAnimals =  await prisma.species.findMany({});
     res.json(allAnimals);
+});
+
+//* Endpoint para obtener un animal por su id
+app.get('/animals/:id', async (req, res) => {
+    const id = req.params.id;
+    const animal = await prisma.species.findUnique({where: {id: parseInt(id)}});
+    res.json(animal);
 });
 
 app.listen(port, () => {
