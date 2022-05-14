9d4503d9864899d941fb81a9c0010fef86c60ac6
Alejandro <alejandrom.am33@gmail.com>
2022-05-14 15:10:47 -0500

    add: endpoint all species

--- server.js
+++ server.js
@@ -3,8 +3,18 @@ const app = express();
 app.use(express.json());
 const port = 3000;
 
-app.get("/", (res, req) => {
-    response.json({message: "Ocean in Danger welcome!"});
+//* Require para usar Prisma
+const { PrismaClient } = require('@prisma/client');
+const { application } = require('express');
+const prisma = new PrismaClient();
+
+app.get('/', (req, res) => {
+    res.json({message: 'alive'});
+});
+
+app.get('/animals', async (req, res) => {
+    const allAnimals =  await prisma.species.findMany({});
+    res.json(allAnimals);
 });
 
 app.listen(port, () => {
