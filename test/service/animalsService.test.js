6566505b462b2248f3b9b5da8307285d33c51b47
Alejandro <alejandrom.am33@gmail.com>
2022-05-15 11:26:49 -0500

    fix: removed console.log

--- test/service/animalsService.test.js
+++ test/service/animalsService.test.js
@@ -6,7 +6,6 @@ describe("Unit test for ExplorerService class", () => {
 
     test("Filter explorers in node mission", () => {
         const animalsByName = AnimalsService.filterByName(animals, "Tiburón");
-        console.log(animalsByName); 
         expect(animalsByName[0].name).toBe("Tiburón Martillo Gigante");
     });
 });
