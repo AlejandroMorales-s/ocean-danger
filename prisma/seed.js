d900cf871004c265df379ee9cf3047d25a5f9795
Alejandro <alejandrom.am33@gmail.com>
2022-05-15 14:17:20 -0500

    add: new animal

--- prisma/seed.js
+++ prisma/seed.js
@@ -4,10 +4,10 @@ const prisma = new PrismaClient();
 (async function main() {
   try {
     const tiburonMartilloGigante = await prisma.species.upsert({
-      where: { name: 'Tiburón Martillo Gigante' },
+      where: { name: 'Giant Hammerhead Shark' },
       update: {},
       create: {
-        name: 'Tiburón Martillo Gigante',
+        name: 'Giant Hammerhead Shark',
         cientificName: 'Sphyrna mokarran',
 		region: 'Golfo de México',
 		minWeight: 230,
@@ -36,10 +36,10 @@ const prisma = new PrismaClient();
       },
     });
     const delfinCabezaBlanca = await prisma.species.upsert({
-      where: { name: 'Delfín Cabeza Blanca' },
+      where: { name: 'White-headed dolphin' },
       update: {},
       create: {
-        name: 'Delfín Cabeza Blanca',
+        name: 'White-headed dolphin',
         cientificName: 'Cephalorhynchus hectori',
 		region: 'Nueva Zelanda',
 		minWeight: 40,
@@ -49,6 +49,22 @@ const prisma = new PrismaClient();
 		lifeExpectancy: '20 years',
     url: "https://i.pinimg.com/originals/f9/53/a9/f953a96a525edacc35fb6f503edd3463.jpg",
     description: "The Hector's dolphin is the smallest member of the family Delphinidae. Adults range in length from 1.2 to 1.6 m and weigh 40 to 60 kg.3 The species has slight sexual dimorphism, with females being slightly larger and heavier than males. The body shape is robust with no distinguishable beak or snout. The Hector's dolphin has a rounded dorsal fin and concave edges. The overall color is a pale gray but closer inspection reveals a complex chromatic upholstery. The forehead is gray with black highlights and the tip of the snout is black. The chest and belly are white, and on them a dark gray line runs from the flippers to the eyes. Another band runs from the sides to the dorsal fin."
+      },
+    });
+    const rorcualComun = await prisma.species.upsert({
+      where: { name: 'Rorual Comun' },
+      update: {},
+      create: {
+        name: 'Rorual Comun',
+        cientificName: 'Balaenoptera physalus',
+		region: 'west of Corsica, Sardinia, Sicily and in the Ligurian Sea.',
+		minWeight: 1800,
+		maxWeight: 17000,
+		color: 'Dark Gray',
+		diet: 'Ominvore',
+		lifeExpectancy: '80 and 90 years',
+    url: "https://www.chiledesarrollosustentable.cl/wp-content/uploads/2018/12/ballena-rorcual-III.jpg",
+    description: "The fin whale has an elongated and hydrodynamic body with a falciform dorsal fin located on the posterior third of the body. The caudal fin ends sharply and has a gray crest. The average length is usually 18 meters. Females are longer than males and can reach a length of about 19 meters. They can weigh up to 90 tons. The fin whale has a dark gray back and sides and a white belly. Their baleen has 260 to 480 blades on each side of the mouth, dark almost black in color, except for those in front of the right side, which are white. The ventral folds reach approximately to the navel and range from 50 to 100. The most notable characters that differentiate them from other species are the asymmetrical coloration of the left lower jaw, which is usually darker than the right, the white supra-mandibular spot and the V-shaped spot between the two spiracle openings; as well as the scars on the posterior margin of the caudal fin and the wounds along the body due to confrontations with other individuals. Another distinctive feature is the blow that can form a column of a height between 4 and 6 meters."
       },
     });
     
